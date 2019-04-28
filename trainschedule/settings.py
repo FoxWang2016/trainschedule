8609b5005677144dc18ba45c970f0d8f6f1e7ae9
Daniel Wang <wangxixi2018@sina.com>
2019-04-28 23:45:55 +0800

    设置爬虫配置

--- trainschedule/settings.py
+++ trainschedule/settings.py
@@ -16,10 +16,18 @@ NEWSPIDER_MODULE = 'trainschedule.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'trainschedule (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36'
+USER_AGENTS = [
+   'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) '
+   'Version/5.1 Safari/534.50',
+   'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36',
+   'User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)',
+   'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+   'User-Agent:Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko)'
+   ' Version/5.1 Safari/534.50']
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,13 +35,15 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
+DOWNLOAD_TIMEOUT = 30
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -88,3 +98,23 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# 指定数据库的主机IP
+REDIS_HOST = "132.232.60.239"
+# 指定数据库的端口号
+REDIS_PORT = 7379
+# redis密码
+REDIS_PASSWORD = {'password': 'ThisIsASecurityRedis', }
+# redis代理池
+REDIS_IP_PROXY_NAME = 'common:ip_proxy'
+# redis 的db号
+REDIS_DB_NUM = 0
+
+#mongo所在主机ip
+MONGO_HOST = "132.232.60.239"
+#mongo所在端口号
+MONGO_PORT = 27017
+#mongo数据库句柄
+MONGO_DB = "sinaWeibo"
+#collection句柄
+MONGO_COLL = "user"
