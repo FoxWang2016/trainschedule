d850603e59c21a082df84bdcf9459b7b9382e64d
Daniel Wang <wangxixi2018@sina.com>
2019-05-08 21:00:31 +0800

    车次信息爬取

--- trainschedule/spiders/trainNumber.py
+++ trainschedule/spiders/trainNumber.py
@@ -1,6 +1,7 @@
 
 # -*- coding: utf-8 -*-
 import json
+import ast
 
 import scrapy
 
@@ -11,6 +12,12 @@ class TrainnumberSpider(scrapy.Spider):
     start_urls = ['https://kyfw.12306.cn/otn/resources/js/query/train_list.js']
 
     def parse(self, response):
-        train_number = response.text.split('=')[1].split()
-        tr_json = json.load(train_number)
-        print(tr_json)
+        all_data = response.text.split('=')[1]
+        data_json = json.loads(all_data)
+        for date_key in data_json:
+            train_number = data_json.get(date_key)
+            for train_type in train_number:
+                station_info = train_number.get(train_type)
+                for train_code in station_info:
+                    print(date_key, "   ", train_type, "   ", train_code.get("station_train_code"), "   "
+                          , train_code.get("train_no"))
